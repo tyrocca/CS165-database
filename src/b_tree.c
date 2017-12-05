f7cf20b0f8bb10908cdaf125ae2c27b9f85a869b
tyrocca <tyler.rocca@gmail.com>
2017-12-05 00:00:27 -0500

    cleanup

--- src/b_tree.c
+++ src/b_tree.c
@@ -43,28 +43,9 @@ BPTNode* pop(BPTNodeStack* stack) {
 }
 
 
-// IDK WHAT to do with this
-typedef struct BPTNodeVal {
-    BPTNode* left_branch;
-    BPTNode* right_branch;
-    BPTNode* parent;
-    size_t value_position;
-    int point_value;
-} BPTNodeVal;
-
-
-void value_to_node(BPTNodeVal* return_obj, BPTNode* bt_node, size_t idx) {
-    return_obj->parent = bt_node;
-    return_obj->point_value = return_obj->parent->node_vals[idx];
-    if (return_obj->parent->is_leaf) {
-        return_obj->left_branch = return_obj->parent->bpt_meta.bpt_ptrs.children[idx];
-        return_obj->right_branch = return_obj->parent->bpt_meta.bpt_ptrs.children[idx + 1];
-    } else {
-        return_obj->left_branch = return_obj->right_branch = NULL;
-        return_obj->point_value = return_obj->parent->bpt_meta.bpt_leaf.col_pos[idx];
-    }
-}
-
+/// **************************************************************************
+/// Creation functions
+/// **************************************************************************
 
 /**
  * @brief Function that creates a node
@@ -95,70 +76,10 @@ BPTNode* create_leaf() {
     return allocate_node(true);
 }
 
-void print_node(BPTNode* node) {
-    printf("[ ");
-    for (size_t i = 0; i < node->num_elements; i++) {
-        printf("%d ", node->node_vals[i]);
-    }
-    printf("]\n");
-}
 
-/* #define GET_NODE_POSITION (node, idx) (node->bpt_meta. */
-void print_leaf(BPTNode* node) {
-    printf("[ ");
-    for (size_t i = 0; i < node->num_elements; i++) {
-        printf(
-            "(%d, %zu) ",
-            node->node_vals[i],
-            node->bpt_meta.bpt_leaf.col_pos[i]
-        );
-    }
-    printf("]\n");
-}
-
-bool is_child(BPTNode* parent, BPTNode* child) {
-    for (size_t i = 0; i < parent->num_elements; i++) {
-        if (parent->bpt_meta.bpt_ptrs.children[i] == child) {
-            return true;
-        }
-    }
-    return false;
-}
-
-void print_tree(BPTNode* node) {
-    BPTNode** all_nodes = malloc(sizeof(BPTNode*) * 2);
-    size_t num_nodes = 1;
-    size_t node_idx = 0;
-    all_nodes[node_idx] = node;
-    /* BPTNode* parent = node; */
-
-    while (node_idx < num_nodes) {
-        // if there are bpt_ptrs.children nodes
-        // copy the node into a current node
-        BPTNode* current_node = all_nodes[node_idx];
-        // if we have have a node with bpt_ptrs.children
-        if (current_node->is_leaf == false) {
-            // these are the number of nodes that we will be adding
-            size_t to_add = current_node->num_elements + 1;
-            // increase our queue of nodes, it will now be equal to the length
-            // currently plus
-            all_nodes = realloc(all_nodes, sizeof(BPTNode*) * (num_nodes + to_add));
-            // copy to the end of the array
-            memcpy(
-                (void*) &all_nodes[num_nodes],
-                (void*) current_node->bpt_meta.bpt_ptrs.children,
-                sizeof(BPTNode*) * to_add
-            );
-            // increase number of nodes
-            num_nodes += to_add;
-        }
-        print_node(current_node);
-        node_idx++;
-    }
-    free(all_nodes);
-    printf("\n");
-}
-
+/// **************************************************************************
+/// Helper functions
+/// **************************************************************************
 
 /**
  * @brief Binary Search function for searching a leaf
@@ -192,29 +113,158 @@ int binary_search(int* arr, int l_pos, int r_pos, int x) {
    return -1;
 }
 
-
-
-typedef struct Result {
-    size_t num_tuples;
-    size_t capacity;
-    /* DataType data_type; */
-    void *payload;
-    /* bool free_after_use; */
-} Result;
+/**
+ * @brief Function that tells you whether a node is the parent of another node
+ *
+ * @param parent - a node at a higher level
+ * @param child - a node at a lower level
+ *
+ * @return bool whether they are ancestors
+ */
+bool is_child(BPTNode* parent, BPTNode* child) {
+    for (size_t i = 0; i < parent->num_elements; i++) {
+        if (parent->bpt_meta.bpt_ptrs.children[i] == child) {
+            return true;
+        }
+    }
+    return false;
+}
 
 /**
- * @brief This finds the values in a BPT
+ * @brief Function that prints out the values of a PointerNode
+ *  print results like [ val, val, val ]
  *
- * @param bt_node
- * @param value
- *
- * @return
+ * @param node - node to print
  */
+void print_node(BPTNode* node) {
+    printf("[ ");
+    for (size_t i = 0; i < node->num_elements; i++) {
+        printf("%d ", node->node_vals[i]);
+    }
+    printf("]\n");
+}
+
+/**
+ * @brief Function that prints out a leaf node.
+ *  prints [ (key, data), (key, data) ]
+ *
+ * @param node - node to print
+ */
+void print_leaf(BPTNode* node) {
+    printf("[ ");
+    for (size_t i = 0; i < node->num_elements; i++) {
+        printf(
+            "(%d, %zu) ",
+            node->node_vals[i],
+            node->bpt_meta.bpt_leaf.col_pos[i]
+        );
+    }
+    printf("]\n");
+}
+
+/**
+ * @brief Function that prints a tree from a given node
+ *      there should be a new line for each level (does
+ *      printing in a BFS manner
+ *
+ * @param node - node to print
+ */
+void print_tree(BPTNode* node) {
+    BPTNode** all_nodes = malloc(sizeof(BPTNode*) * 2);
+    size_t num_nodes = 1;
+    size_t node_idx = 0;
+    all_nodes[node_idx] = node;
+
+    while (node_idx < num_nodes) {
+        // if there are bpt_ptrs.children nodes
+        // copy the node into a current node
+        BPTNode* current_node = all_nodes[node_idx];
+        // if we have have a node with bpt_ptrs.children
+        if (current_node->is_leaf == false) {
+            // these are the number of nodes that we will be adding
+            size_t to_add = current_node->num_elements + 1;
+            // increase our queue of nodes, it will now be equal to the length
+            // currently plus
+            all_nodes = realloc(all_nodes, sizeof(BPTNode*) * (num_nodes + to_add));
+            // copy to the end of the array
+            memcpy(
+                (void*) &all_nodes[num_nodes],
+                (void*) current_node->bpt_meta.bpt_ptrs.children,
+                sizeof(BPTNode*) * to_add
+            );
+            // increase number of nodes
+            num_nodes += to_add;
+        }
+        print_node(current_node);
+        node_idx++;
+    }
+    free(all_nodes);
+    printf("\n");
+}
+
+/**
+ * @brief Function that tests our print
+ */
+void testing_print() {
+    BPTNode* root = create_node();
+    root->num_elements = 2;
+    root->node_vals[0] = 0;
+    root->node_vals[1] = 10;
+
+    BPTNode* left = create_node();
+    root->bpt_meta.bpt_ptrs.children[0] = left;
+    left->num_elements = 1;
+    left->node_vals[0] = -1;
+
+    BPTNode* asdf = create_leaf();
+    left->bpt_meta.bpt_ptrs.children[0] = asdf;
+    asdf->num_elements = 1;
+    asdf->node_vals[0] = -15;
+    asdf->bpt_meta.bpt_leaf.col_pos[0] = 225;
+
+    BPTNode* qwer = create_leaf();
+    left->bpt_meta.bpt_ptrs.children[1] = qwer;
+    qwer->num_elements = 1;
+    qwer->node_vals[0] = -30;
+    qwer->bpt_meta.bpt_leaf.col_pos[0] = 900;
 
 
+    BPTNode* middle = create_leaf();
+    root->bpt_meta.bpt_ptrs.children[1] = middle;
+    middle->num_elements = 1;
+    middle->node_vals[0] = 5;
+    middle->bpt_meta.bpt_leaf.col_pos[0] = 25;
+
+    BPTNode* right = create_leaf();
+    root->bpt_meta.bpt_ptrs.children[2] = right;
+    right->num_elements = 1;
+    right->node_vals[0] = 200;
+    middle->bpt_meta.bpt_leaf.col_pos[0] = 40000;
+
+    print_tree(root);
+
+    free(right);
+    free(middle);
+    free(qwer);
+    free(asdf);
+    free(left);
+    free(root);
+}
+
+
+/// ***************************************************************************
+/// Searching Functions
+/// ***************************************************************************
+
+/**
+ * @brief This function increases the size of a result array
+ *      TODO: make it so the function can increase using an actual
+ *      result from the database
+ *
+ * @param result
+ */
 void increase_result_array(Result *result) {
     result->capacity *= 2;
-    // FIXME - change to correct type
     result->payload = realloc(result->payload, sizeof(size_t) * result->capacity);
 }
 
@@ -225,29 +275,24 @@ void add_to_results(Result* result, size_t value) {
     // add the value to the array
     ((size_t*)result->payload)[result->num_tuples++] = value;
 }
+/* size_t find_value(BPTNode* bt_node, int value, Result* result); */
 
-/* size_t find_value(BPTNode* bt_node, int value, Result* result) { */
-/*     if (bt_node->is_leaf) { */
-/*         for (size_t i = 0; i < bt_node->num_elements; i++) { */
-/*             if (bt_node->node_vals[i] == value) { */
-/*                 add_to_results(result, */
 
-/*             } */
+/// **************************************************************************
+/// Insertion Functions
+/// **************************************************************************
 
-/*         for (size */
-/*     } */
-/*     /1* // iterate over data *1/ */
-
-/*     /1* for (size_t i = 0; i < bt_node->num_elements; i++) { *1/ */
-/*     /1*     if value is a node value *1/ */
-/*     /1*         check the lef *1/ */
-
-/*     /1* } *1/ */
-/*     return 0; */
-/* } */
-
+/**
+ * @brief Function for adding (key,value) to a position (once we know that
+ *      the leaf has space
+ *
+ * @param bt_node - node to add to - cannot be full
+ * @param value - value to add
+ * @param position - position to add
+ */
 void insert_into_leaf(BPTNode* bt_node, int value, size_t position) {
     // start at the first value
+    assert(bt_node->num_elements < MAX_KEYS);
     size_t i = 0;
     while (i < bt_node->num_elements) {
         // if the key is larger or if they are equal and the position is
@@ -300,16 +345,6 @@ void test_leaf_insert() {
     free(node2);
 }
 
-void find_leaf(BPTNode* bt_node, int value, BPTNodeStack* access_list) {
-    /* for ( */
-
-}
-
-typedef struct SplitNode {
-    BPTNode* left_leaf;
-    BPTNode* right_leaf;
-    int middle_val;
-} SplitNode;
 
 /**
  * @brief Function that takes in a full leaf and a next value and
@@ -402,6 +437,13 @@ void test_split_leaf() {
     free(node);
 }
 
+/**
+ * @brief Function for adding a key, value pair to a leaf
+ *
+ * @param bt_node - node to add to
+ * @param value - value to add to the node
+ * @param position - position
+ */
 void add_to_leaf(BPTNode* bt_node, int value, size_t position) {
     assert(bt_node->is_leaf == true);
     // we can either do a naive insert or we
@@ -416,65 +458,32 @@ void add_to_leaf(BPTNode* bt_node, int value, size_t position) {
     }
 }
 
-
+/**
+ * @brief Function for inserting into a tree
+ *
+ * @param bt_node
+ * @param value
+ * @param position
+ */
 void insert_value(BPTNode* bt_node, int value, size_t position) {
+    (void) bt_node;
+    (void) value;
+    (void) position;
 }
 
-void rebalance();
-void delete();
+/// ***************************************************************************
+/// B Plus Tree Body Insertions
+/// ***************************************************************************
 
-
-
-void testing_print() {
-    BPTNode* root = create_node();
-    root->num_elements = 2;
-    root->node_vals[0] = 0;
-    root->node_vals[1] = 10;
-
-    BPTNode* left = create_node();
-    root->bpt_meta.bpt_ptrs.children[0] = left;
-    left->num_elements = 1;
-    left->node_vals[0] = -1;
-
-    BPTNode* asdf = create_leaf();
-    left->bpt_meta.bpt_ptrs.children[0] = asdf;
-    asdf->num_elements = 1;
-    asdf->node_vals[0] = -15;
-    asdf->bpt_meta.bpt_leaf.col_pos[0] = 225;
-
-    BPTNode* qwer = create_leaf();
-    left->bpt_meta.bpt_ptrs.children[1] = qwer;
-    qwer->num_elements = 1;
-    qwer->node_vals[0] = -30;
-    qwer->bpt_meta.bpt_leaf.col_pos[0] = 900;
-
-
-    BPTNode* middle = create_leaf();
-    root->bpt_meta.bpt_ptrs.children[1] = middle;
-    middle->num_elements = 1;
-    middle->node_vals[0] = 5;
-    middle->bpt_meta.bpt_leaf.col_pos[0] = 25;
-
-    BPTNode* right = create_leaf();
-    root->bpt_meta.bpt_ptrs.children[2] = right;
-    right->num_elements = 1;
-    right->node_vals[0] = 200;
-    middle->bpt_meta.bpt_leaf.col_pos[0] = 40000;
-
-    print_tree(root);
-
-    free(right);
-    free(middle);
-    free(qwer);
-    free(asdf);
-    free(left);
-    free(root);
+void add_to_pointer_node(SplitNode split_node){
+    (void) split_node;
 }
+
+
+
 
 int main(void) {
     printf("Testing leaf insert\n");
     test_split_leaf();
-
-
     return 0;
 }
--- src/b_tree.h
+++ src/b_tree.h
@@ -1,27 +1,38 @@
 #ifndef BPLUS_TREE_H
 #define BPLUS_TREE_H
-
+// Libraries
 #include <stdlib.h>
+#include <stdbool.h>
 
 /* #include "cs165_api.h" */
 
-// This file contains all the b-tree stuff...
+/// ***************************************************************************
+/// B Tree Initialization variables
+/// ***************************************************************************
+
 // MAX is 340, half is 170
 #define MIN_DEGREE 2  // Min number of pointers in a level
 #define MIN_KEYS (MIN_DEGREE - 1)  // Minimum number of keys in a node
 #define MAX_KEYS ((2 * MIN_DEGREE) - 1)  // Max num of keys in a node
 /* #define MAX_KEYS 2 */
-#define LEFT_SPLIT (MAX_KEYS / 2)
-#define RIGHT_SPLIT (LEFT_SPLIT - MAX_KEYS)
+#define LEFT_SPLIT (MAX_KEYS / 2) // size of a right split
+#define RIGHT_SPLIT (LEFT_SPLIT - MAX_KEYS) // size of a left split
 #define MAX_DEGREE (2 * MIN_DEGREE)  // Max num pointers from a node
 
 #define RESULT_SCALING 16
 
-typedef enum BPTREE_OP {
-    INSERT_VAL,
-    DELETE_VAL,
-    FIND_VAL
-} BPTREE_OP;
+/// ***************************************************************************
+/// B Plus Tree Types
+/// ***************************************************************************
+
+// TEMP - until integrated
+typedef struct Result {
+    size_t num_tuples;
+    size_t capacity;
+    /* DataType data_type; */
+    void *payload;
+    /* bool free_after_use; */
+} Result;
 
 // Define the "BPTNode"
 struct BPTNode;
@@ -33,11 +44,9 @@ struct BPTNode;
  *      - Pointer to the previous leaf
  */
 typedef struct BPTLeaf {
-    // a child node
-    // the 'fence' contains the degree plus 1 for the number of fences
-    size_t col_pos[MAX_KEYS];
-    struct BPTNode* next_leaf;
-    struct BPTNode* prev_leaf;
+    size_t col_pos[MAX_KEYS];  // these are the positions
+    struct BPTNode* next_leaf; // this is the next pointer (next leaf)
+    struct BPTNode* prev_leaf; // this is the previous pointer (previous leaf)
 } BPTLeaf;
 
 /**
@@ -45,11 +54,9 @@ typedef struct BPTLeaf {
  * have n+1 of them
  */
 typedef struct BPTPointers {
-    // the positions contain the node degree number of positions if we have
-    // a child node
-    struct BPTNode* children[MAX_DEGREE];
-    bool is_head;
-    unsigned int level;
+    struct BPTNode* children[MAX_DEGREE];  // Array of pointers to childern
+    bool is_root;                          // Bool to indicate if is root
+    unsigned int level;                    // Level of the tree
 } BPTPointers;
 
 /**
@@ -69,7 +76,7 @@ typedef union BPTMeta {
 typedef struct BPTNode {
     BPTMeta bpt_meta;            // This contains the node details
     size_t num_elements;         // this is the currently used size of the array
-    int node_vals[MAX_KEYS];  // these are the datapoints
+    int node_vals[MAX_KEYS];     // these are the datapoints
     bool is_leaf;                // this tells us the type
 } BPTNode;
 
@@ -80,6 +87,83 @@ typedef struct BPTNodeStack {
     BPTNode** array;
 } BPTNodeStack;
 
+// IDK WHAT to do with this
+typedef struct BPTNodeVal {
+    BPTNode* left_branch;
+    BPTNode* right_branch;
+    BPTNode* parent;
+    size_t value_position;
+    int point_value;
+} BPTNodeVal;
 
+/**
+ * @brief This structure is used to hold the result of splitting a leaf node
+ */
+typedef struct SplitNode {
+    BPTNode* left_leaf;    // This is the left node resulting from the split
+    BPTNode* right_leaf;   // this is the right node (results from split)
+    int middle_val;        // this is the middle value (results from split)
+} SplitNode;
+
+
+/* void value_to_node(BPTNodeVal* return_obj, BPTNode* bt_node, size_t idx) { */
+/*     return_obj->parent = bt_node; */
+/*     return_obj->point_value = return_obj->parent->node_vals[idx]; */
+/*     if (return_obj->parent->is_leaf) { */
+/*         return_obj->left_branch = return_obj->parent->bpt_meta.bpt_ptrs.children[idx]; */
+/*         return_obj->right_branch = return_obj->parent->bpt_meta.bpt_ptrs.children[idx + 1]; */
+/*     } else { */
+/*         return_obj->left_branch = return_obj->right_branch = NULL; */
+/*         return_obj->point_value = return_obj->parent->bpt_meta.bpt_leaf.col_pos[idx]; */
+/*     } */
+/* } */
+
+// ****************************************************************************
+// NODE Functions
+// ****************************************************************************
+
+// allocation functions
+BPTNode* allocate_node(bool leaf);
+BPTNode* create_node();
+BPTNode* create_leaf();
+
+
+// ****************************************************************************
+// Helper Functions - used for debugging / general things
+// ****************************************************************************
+int binary_search(int* arr, int l_pos, int r_pos, int x);
+bool is_child(BPTNode* parent, BPTNode* child);
+void print_node(BPTNode* node);
+void print_leaf(BPTNode* node);
+void print_tree(BPTNode* node);
+void testing_print();
+
+
+/// ***************************************************************************
+/// Searching Functions
+/// ***************************************************************************
+void increase_result_array(Result *result);
+void add_to_results(Result* result, size_t value);
+size_t find_value(BPTNode* bt_node, int value, Result* result);
+
+
+/// **************************************************************************
+/// B Plus Tree Insertion Functions
+/// **************************************************************************
+
+void insert_into_leaf(BPTNode* bt_node, int value, size_t position);
+void test_leaf_insert();
+
+void split_leaf(BPTNode* bt_node, int value, size_t pos, SplitNode* split_node);
+void test_split_leaf();
+
+// FIXME;
+void add_to_leaf(BPTNode* bt_node, int value, size_t position);
+
+// TODO:
+void insert_value(BPTNode* bt_node, int value, size_t position);
+
+// TODO:
+void add_to_pointer_node(SplitNode);
 
 #endif
